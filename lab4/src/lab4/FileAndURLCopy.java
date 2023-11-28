6025ebdd5f9bc20cdaece38092fa30ccdbe184d8
kadray <1janekguz1@gmail.com>
2023-11-28 19:16:44 +0100

    lab4 done

--- lab4/src/lab4/FileAndURLCopy.java
+++ lab4/src/lab4/FileAndURLCopy.java
@@ -4,51 +4,110 @@ import java.net.*;
 
 public class FileAndURLCopy {
     public static void main(String[] args) {
-        if (args.length < 1 || args.length > 2) {
-            System.out.println("Sposób użycia: java FileAndURLCopy <adres_URL> [<nazwa_pliku_docelowego>]");
+        if (args.length < 1) {
+            System.out.println("Brak argumentów programu.");
             return;
         }
 
-        String sourceURL = args[0];
-        String destinationFile = "";
-
-        if (args.length == 2) {
-            destinationFile = args[1];
-        } else {
-            try {
-                URL url = new URL(sourceURL);
-                String[] pathSegments = url.getPath().split("/");
-                destinationFile = pathSegments[pathSegments.length - 1];
-            } catch (MalformedURLException e) {
-                System.out.println("Podany adres: '" + sourceURL + "' jest nieprawidłowy.");
-                return;
+        String source_path = args[0];
+        String end_path = "copy";
+        if(isURL(source_path)) end_path+=".html";
+        else end_path+=".txt";
+        
+        if(args.length == 2) {
+        	end_path = args[1];
+        }
+        try {
+            if (isURL(source_path)) {
+                copyFromURL(source_path, end_path);
+            } else {
+                copyFromFile(source_path, end_path);
             }
+        } catch (IOException e) {
+            System.out.println("Wystąpił błąd: " + e.getMessage());
+        }
+    }
+
+    private static boolean isURL(String path) {
+        return path.toLowerCase().startsWith("http://") || path.toLowerCase().startsWith("https://");
+    }
+
+    private static void copyFromFile(String sourcePath, String targetPath) throws IOException {
+        File sourceFile = new File(sourcePath);
+        File targetFile = new File(targetPath);
+
+        if (!sourceFile.exists()) {
+            System.out.println("Plik " + sourcePath + " nie istnieje.");
+            return;
         }
 
+        if (sourceFile.isDirectory()) {
+            System.out.println(sourcePath + " jest katalogiem.");
+            return;
+        }
+
+        if (!sourceFile.canRead()) {
+            System.out.println("Brak dostępu do pliku " + sourcePath);
+            return;
+        }
+
+        if (targetFile.isDirectory()) {
+            targetFile = new File(targetFile, sourceFile.getName());
+        }
+
+        if (targetFile.exists() && !targetFile.canWrite()) {
+            System.out.println("Brak wymaganych uprawnień do zapisu pliku " + targetPath);
+            return;
+        }
+
+        if (targetFile.exists() && !targetFile.delete()) {
+            System.out.println("Nie można nadpisać pliku " + targetPath);
+            return;
+        }
+
+        try (InputStream in = new FileInputStream(sourceFile);
+             OutputStream out = new FileOutputStream(targetFile)) {
+
+            byte[] buffer = new byte[1024];
+            int length;
+
+            while ((length = in.read(buffer)) > 0) {
+                out.write(buffer, 0, length);
+            }
+
+            System.out.println("Plik został skopiowany pomyślnie.");
+        }
+    }
+
+    private static void copyFromURL(String sourceURL, String targetPath) {
         try {
             URL url = new URL(sourceURL);
             HttpURLConnection connection = (HttpURLConnection) url.openConnection();
+
             int responseCode = connection.getResponseCode();
+
             if (responseCode == HttpURLConnection.HTTP_OK) {
-                InputStream inputStream = connection.getInputStream();
-                FileOutputStream outputStream = new FileOutputStream(destinationFile);
+                try (InputStream in = connection.getInputStream();
+                     OutputStream out = new FileOutputStream(targetPath)) {
 
-                byte[] buffer = new byte[1024];
-                int bytesRead;
-                while ((bytesRead = inputStream.read(buffer)) != -1) {
-                    outputStream.write(buffer, 0, bytesRead);
+                    byte[] buffer = new byte[1024];
+                    int length;
+
+                    while ((length = in.read(buffer)) > 0) {
+                        out.write(buffer, 0, length);
+                    }
+
+                    System.out.println("Plik z URL został skopiowany pomyślnie.");
                 }
-
-                outputStream.close();
-                inputStream.close();
-                System.out.println("Plik skopiowany pomyślnie jako: " + destinationFile);
             } else {
-                System.out.println("Brak dostępu do '" + sourceURL + "'. Kod odpowiedzi: " + responseCode);
+                System.out.println("Nie udało się pobrać pliku z URL. Kod odpowiedzi: " + responseCode);
             }
+        } catch (UnknownHostException e) {
+            System.out.println("Brak połączenia siecowego");
         } catch (MalformedURLException e) {
-            System.out.println("Podany adres: '" + sourceURL + "' jest nieprawidłowy.");
+            System.out.println("Podany adres: "+ sourceURL + " jest nieprawidłowy");
         } catch (IOException e) {
-            System.out.println("Brak połączenia sieciowego.");
+            System.out.println("Brak dostępu do " + sourceURL);
         }
     }
 }
