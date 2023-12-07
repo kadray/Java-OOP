0b005c95e23d2c690ea75947dde0f2a223609d81
kadray <1janekguz1@gmail.com>
2023-12-07 21:25:49 +0100

    changes in lab5

--- lab5/src/lab5/MultiplicationTable.java
+++ lab5/src/lab5/MultiplicationTable.java
@@ -12,16 +12,16 @@ public class MultiplicationTable {
 		try(FileReader properties_read = new FileReader(prop_name)) {
 			properties.load(properties_read);
 			}catch(IOException e) {
-				properties.setProperty("wartosc_maksimum", "10");
+				properties.setProperty("wartosc_maximum", "10");
 				properties.setProperty("wartosc_minimum", "1");
 				properties.setProperty("procent", "0.7");
-				properties.setProperty("powtorzen_maksimum", "25");
+				properties.setProperty("powtorzen_maximum", "25");
 				properties.setProperty("powtorzen_minimum", "10");
 				try {
 					FileOutputStream properties_out = new FileOutputStream(prop_name);
 					properties.store(properties_out, null);
-				}catch(IOException e2) {
-					System.err.println(e2.getMessage());
+				}catch(IOException ee) {
+					System.err.println(ee.getMessage());
 					return;
 				}
 			}
@@ -31,31 +31,31 @@ public class MultiplicationTable {
     }
 
     public static int getMaxValue() {
-        return Integer.parseInt(properties.getProperty("wartosc_maksimum"));
+        return Integer.parseInt(properties.getProperty("wartosc_maximum"));
     }
 
     public static float getPercent() {
         return Float.parseFloat(properties.getProperty("procent"));
     }
 
-    public static int getMinRepeats() {
-        return Integer.parseInt(properties.getProperty("powtorzen_minimum"));
+    public static int getMaxRepeats() {
+        return Integer.parseInt(properties.getProperty("powtorzen_maximum"));
     }
 
-    public static int getMaxRepeats() {
-        return Integer.parseInt(properties.getProperty("powtorzen_maksimum"));
+    public static int getMinRepeats() {
+        return Integer.parseInt(properties.getProperty("powtorzen_minimum"));
     }
     
     
 	public static void main(String[] args) {
-		MultiplicationTable test1 = new MultiplicationTable();
+		MultiplicationTable m = new MultiplicationTable();
 		int rounds=1;
 		int good_answer=0;
 		Random rand_val = new Random();
 		Scanner user_input = new Scanner(System.in);
-		while(rounds <= test1.getMaxRepeats()) {
-			int val_1=rand_val.nextInt(test1.getMinValue(), test1.getMaxValue());
-			int val_2=rand_val.nextInt(test1.getMinValue(), test1.getMaxValue());
+		while(rounds <= m.getMaxRepeats()) {
+			int val_1=rand_val.nextInt(m.getMinValue(), m.getMaxValue());
+			int val_2=rand_val.nextInt(m.getMinValue(), m.getMaxValue());
 			int answer=0;
 			
 			System.out.println(val_1 + " * " + val_2+ " =");
@@ -69,7 +69,7 @@ public class MultiplicationTable {
 			
 			if(answer == val_1*val_2) good_answer++;
 			
-			if((float)good_answer/rounds>=test1.getPercent() && rounds>=test1.getMinRepeats()) {
+			if((float)good_answer/rounds>=m.getPercent() && rounds>=m.getMinRepeats()) {
 				System.out.println("Koniec gry, liczba rund: "+rounds);
 				System.out.println("Oddałeś " + ((float)good_answer/rounds)*100 + "% poprawnych odpowiedzi");
 				return;
